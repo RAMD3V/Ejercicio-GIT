f8f519e9ff6e5b497e31897b6b5fee3c20ba6ddf
RaMoVa <ramon.molina.v9@gmail.com>
2022-02-11 13:48:38 -0600

    Cambio de texto y color en botones

--- js/jsbotones.js
+++ js/jsbotones.js
@@ -7,13 +7,25 @@ window.addEventListener("load", function(){
 
     btn1.addEventListener("click",function(){
         btn1.disabled = true;
+        btn2.disabled = false;
+        btn1.style.backgroundColor= "#DC5395";
+        btn1.style.color= "white";
+        btn1.textContent="Botòn cambiado a Rosa";
     });
     btn2.addEventListener("click",function(){
         btn2.disabled = true;
+        btn3.disabled = false;
+        btn2.style.backgroundColor= "#3155B1";
+        btn2.style.color= "white";
+        btn2.textContent="Botòn cambiado a Azul";
     });
     btn3.addEventListener("click",function(){
         btn3.disabled = true;
+        btn1.disabled = false;
+        btn3.style.backgroundColor= "#C675EE";
+        btn3.style.color= "white";
+        btn3.textContent="Botòn cambiado a Morado";
     });
-
+   
 
 });
