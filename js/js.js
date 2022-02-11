048a72ee99949554a697dcec8aa0eecde7af19df
RaMoVa <ramon.molina.v9@gmail.com>
2022-02-11 12:05:16 -0600

    Funcionalidad de dormulario

--- js/js.js
+++ js/js.js
@@ -1,4 +1,29 @@
-function cambiarFondo(){
-    document.body.style.backgroundColor = '#B075EE';
-    alert("El color de fondo cambiará");
-}
+"use strict"
+window.addEventListener("load",function(){
+    function cambiarFondo(){
+        document.body.style.backgroundColor = '#B075EE';
+        alert("El color de fondo cambiará");
+    }
+    var formulario=document.querySelector("#formulario");
+    formulario.addEventListener('submit', function(){
+        console.log("Captura de datos");
+        var nombre= document.querySelector("#form_name").value;
+        var email= document.querySelector("#email").value;
+        var edad= parseInt(document.querySelector("#edad").value);
+        
+        var datos=document.querySelector(".datos");
+        datos.style.display="block";
+        var d_nombre = document.querySelector("#d_nombre span");
+        var d_email = document.querySelector("#d_email span");
+        var d_edad = document.querySelector("#d_edad span");
+        
+        console.log(d_edad);
+        console.log(d_email);
+        console.log(d_nombre);
+        d_nombre.innerHTML = nombre;
+        d_email.innerHTML = email;
+        d_edad.innerHTML = edad;
+    
+    });
+    
+});
